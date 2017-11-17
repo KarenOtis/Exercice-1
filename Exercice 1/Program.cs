72939d3671dcfdafbcb66afa2762520a470715c7
cfib9 <cfib9@A3019-09>
2017-11-17 12:01:29 -0500

    12h01

--- Exercice 1/Program.cs
+++ Exercice 1/Program.cs
@@ -10,6 +10,47 @@ namespace Exercice_1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Quel est ton nom?");
+            String nom = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Quel est ton prénom?");
+            String prenom = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Quelle est ta date de naissance?");
+            DateTime x = DateTime.Parse(Console.ReadLine());
+            DateTime y = DateTime.Now;
+            TimeSpan duree = y - x;
+            Console.WriteLine();
+
+            Console.WriteLine("Écrire un nombre entre 0 et 100.");
+            int nb1 = int.Parse(Console.ReadLine());
+            bool nb3 = nb1 > 50;
+            Console.WriteLine();
+
+            Console.WriteLine("Bonjour {0} {1}!", prenom, nom);
+            Console.WriteLine();
+
+            Console.WriteLine("Tu as {0} jours.", duree.Days);
+            Console.WriteLine();
+
+            Console.WriteLine("Ton {0} est supérieur à 50? {1}", nb1, nb3);
+
+            Console.WriteLine("BOOOMMMM");
+
+
+            
+
+
+            
+
+
+
+            
+
+
+
         }
     }
 }
